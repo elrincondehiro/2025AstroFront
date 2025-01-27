33a3bf24d6728d13318ec3547aced3ca9f5a222b
elrincondehiro <administracion@elrincondehiro.com>
2025-01-27 10:17:41 +0100

    initial commit bolt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [tailwind()],
+  // adapter: vercel(),
+  env: {
+    schema:{
+
+    }
+  },
+  // experimental: {
+  //   session: {
+  //     // Required: the name of the Unstorage driver
+  //     driver: "fs",
+  //   },
+  // },
+});
