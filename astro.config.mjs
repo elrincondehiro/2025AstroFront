02af47be9dd7e6b8891b7d41975348a6e92fd56d
elrincondehiro <administracion@elrincondehiro.com>
2025-02-04 17:26:20 +0100

    few changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
+import icon from 'astro-icon';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()],
-  // adapter: vercel(),
-  env: {
-    schema:{
-
-    }
-  },
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  integrations: [tailwind(), svelte({extensions: ['.svelte']}), icon()],
   // experimental: {
   //   session: {
   //     // Required: the name of the Unstorage driver
   //     driver: "fs",
   //   },
   // },
+  env: {
+    schema:{
+
+    }
+  },
 });
